11488e5256f51b4d81a0cc218c4fb2cfbc5ef879
Rishab sharma <rehnirishab@gmail.com>
2026-07-16 09:38:20 +0530

    Create Document SQLAlchemy model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,21 @@
-from sqlalchemy import Column, Integer, String , Numeric
+from sqlalchemy import Column, Integer, String , Numeric, BigInteger, DataTime
 from app.database import Base
+from datetime import datetime
 
 
 
 class Document(Base):
     __tablename__="documents"               # - This has to be there 
+
+    id = Column(Integer, primary_key=True, index=Tablesrue)
+    title= Column(String,nullable=False)
+    original_filename = Column(String,nullable =false)
+    stored_filename = Column(String, nullable=False)
+    content_type = Column(String, nullable=False)
+    file_size = Column(BigInteger, nullable=False)
+    uploaded_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
     
